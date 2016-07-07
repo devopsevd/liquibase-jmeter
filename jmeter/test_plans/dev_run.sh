3c96fd83b5e076ca8dd5bfc93d6600af1309e50a
Brooks Robertson <brobertson@esri.com>
2016-07-07 16:51:14 -0400

    adding utility scripts to run tests in containers from console

--- jmeter/test_plans/dev_run.sh
+++ jmeter/test_plans/dev_run.sh
@@ -1,3 +1,6 @@
-jmeter -n -t $JMETER_HOME/v1_api/datasets_paging_dev.jmx -p $JMETER_HOME/config/user.properties -l /data/data.csv & \
-jmeter -n -t $JMETER_HOME/v2_api/datasets_paging_dev.jmx -p $JMETER_HOME/config/user.properties -l /data/data.csv & \
-jmeter -n -t $JMETER_HOME/v2_api/cloudfront_css_endpoint_test.jmx -p $JMETER_HOME/config/user.properties -J csv_data=$JMETER_HOME/config/data_dev.csv -l /data/data.csv &
+#!/bin/bash
+
+## Run perfs tests against dev environment
+jmeter -n -t $JMETER_HOME/test_plans/v1_api/datasets_paging_dev.jmx -p $JMETER_HOME/config/user.properties -l /data/data.csv & \
+jmeter -n -t $JMETER_HOME/test_plans/v2_api/datasets_paging_dev.jmx -p $JMETER_HOME/config/user.properties -l /data/data.csv & \
+jmeter -n -t $JMETER_HOME/test_plans/v2_api/cloudfront_css_endpoint_test.jmx -p $JMETER_HOME/config/user.properties -J csv_data=$JMETER_HOME/config/data_dev.csv -l /data/data.csv &
