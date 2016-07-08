dc5094c021ee6abd10b55257b09b3b524ca5f8d3
Brooks Robertson <brobertson@esri.com>
2016-07-08 10:53:28 -0400

    updating test runner shell to execute jmeter tests remotely

--- test_runner.sh
+++ test_runner.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
-docker exec -it jelk_jmeter_1 '/bin/bash'
+dev='jmeter -n -t $JMETER_HOME/test_plans/v1_api/datasets_paging_dev.jmx -p $JMETER_HOME/config/user.properties -l /data/data.csv && jmeter -n -t $JMETER_HOME/test_plans/v2_api/datasets_paging_dev.jmx -p $JMETER_HOME/config/user.properties -l /data/data.csv && jmeter -n -t $JMETER_HOME/test_plans/v2_api/cloudfront_css_endpoint_test.jmx -p $JMETER_HOME/config/user.properties -J csv_data=$JMETER_HOME/config/data_dev.csv -l /data/data.csv &'
+
+qa="jmeter -n -t $JMETER_HOME/test_plans/v1_api/datasets_paging_qa.jmx -p $JMETER_HOME/config/user.properties -l /data/data.csv && jmeter -n -t $JMETER_HOME/test_plans/v2_api/datasets_paging_qa.jmx -p $JMETER_HOME/config/user.properties -l /data/data.csv && jmeter -n -t $JMETER_HOME/test_plans/v2_api/cloudfront_css_endpoint_test.jmx -p $JMETER_HOME/config/user.properties -J csv_data=$JMETER_HOME/config/data_qa.csv -l /data/data.csv &"
+
+if [ "$1" == "" ];then
+  docker exec -it jelk_jmeter_1 bash -c 'jmeter -n -t $JMETER_HOME/test_plans/v1_api/datasets_paging_dev.jmx -p $JMETER_HOME/config/user.properties -l /data/data.csv && jmeter -n -t $JMETER_HOME/test_plans/v2_api/datasets_paging_dev.jmx -p $JMETER_HOME/config/user.properties -l /data/data.csv && jmeter -n -t $JMETER_HOME/test_plans/v2_api/cloudfront_css_endpoint_test.jmx -p $JMETER_HOME/config/user.properties -J csv_data=$JMETER_HOME/config/data_dev.csv -l /data/data.csv'
+else
+  docker exec -it jelk_jmeter_1 bash -c 'jmeter -n -t $JMETER_HOME/test_plans/v1_api/datasets_paging_qa.jmx -p $JMETER_HOME/config/user.properties -l /data/data.csv && jmeter -n -t $JMETER_HOME/test_plans/v2_api/datasets_paging_qa.jmx -p $JMETER_HOME/config/user.properties -l /data/data.csv && jmeter -n -t $JMETER_HOME/test_plans/v2_api/cloudfront_css_endpoint_test.jmx -p $JMETER_HOME/config/user.properties -J csv_data=$JMETER_HOME/config/data_qa.csv -l /data/data.csv &'
+fi
+
+
+
