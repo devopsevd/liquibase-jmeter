07693828ba096e659a79c3ae405b4b0a45dd53be
Brooks Robertson <brobertson@esri.com>
2016-08-08 11:39:24 -0400

    removing test shells and plans

--- jmeter/test_plans/qa_run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-## Run perfs tests against QA environment
-jmeter -n -t $JMETER_HOME/test_plans/v1_api/datasets_paging_qa.jmx -p $JMETER_HOME/config/user.properties -l /data/data.csv & \
-jmeter -n -t $JMETER_HOME/test_plans/v2_api/datasets_paging_qa.jmx -p $JMETER_HOME/config/user.properties -l /data/data.csv & \
-jmeter -n -t $JMETER_HOME/test_plans/v2_api/cloudfront_css_endpoint_test.jmx -p $JMETER_HOME/config/user.properties -J csv_data=$JMETER_HOME/config/data_qa.csv -l /data/data.csv &
