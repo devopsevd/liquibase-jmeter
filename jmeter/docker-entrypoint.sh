359a660ae7cffc7015e39117f4649b5bbab9b9dd
Brooks Robertson <brobertson@esri.com>
2016-08-08 12:14:25 -0400

    updated docker post docker install shell script

--- jmeter/docker-entrypoint.sh
+++ jmeter/docker-entrypoint.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 set -e
 tail -f $JMETER_HOME/$JMETER_LOG
+
+docker exec -it jelk_jmeter_1 bash -c \
+'jmeter -n -t $JMETER_HOME/test_plans/test_plan_example.jmx -p $JMETER_HOME/config/user.properties -J csv_data=$JMETER_HOME/config/test_data_example.csv -l /data/test_result.csv'
+
