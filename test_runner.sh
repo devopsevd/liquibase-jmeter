9fa9e89b0eef9e051c80cbdeecd741f15cc5cd16
Brooks Robertson <brobertson@esri.com>
2017-03-12 17:15:19 -0400

    updating configs and test example files

--- test_runner.sh
+++ test_runner.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+
+docker exec -it jelk_jmeter_1 bash -c 'cd /data/; >test_results.csv'
 docker exec -it jelk_jmeter_1 bash -c \
-'jmeter -n -t $JMETER_HOME/test_plans/test_plan_example.jmx -p $JMETER_HOME/config/user.properties -J csv_data=$JMETER_HOME/config/test_data_example.csv -l /data/test_result.csv'
+'jmeter -n -t $JMETER_HOME/test_plans/test_plan_example.jmx -p $JMETER_HOME/config/user.properties -J csv_data=$JMETER_HOME/config/example_urls.csv -l /data/test_results.csv'
 
 
 
