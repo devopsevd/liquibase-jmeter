ecce587b1eea5609c16a9e63ff2b89cdb0ce9f85
devopsevd <devopsevd@users.noreply.github.com>
2018-08-22 19:10:48 +0530

    Update test_runner.sh

--- test_runner.sh
+++ test_runner.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 
-docker exec -it jelk_jmeter_1 bash -c 'cd /data/; >test_results.csv'
-docker exec -it jelk_jmeter_1 bash -c \
+docker exec -it liquibaseappperformancetest_jmeter_1 bash -c 'cd /data/; >test_results.csv'
+docker exec -it liquibaseappperformancetest_jmeter_1 bash -c \
 'jmeter -n -t $JMETER_HOME/test_plans/test_plan_example.jmx -p $JMETER_HOME/config/user.properties -J csv_data=$JMETER_HOME/config/example_urls.csv -l /data/test_results.csv'
 
 
